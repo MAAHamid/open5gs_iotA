de56c78a3284505908daad90fe40d21a6e14105d
abdallah <abdallah.r.waly@orange.com>
2025-01-22 11:23:11 +0200

    update

--- src/sgwc/gtp-path.c
+++ src/sgwc/gtp-path.c
@@ -97,7 +97,7 @@ static void _gtpv2_c_recv_cb(short when, ogs_socket_t fd, void *data)
             char *ip = OGS_ADDR(&from, frombuf);
             if(strcmp(ip, "10.131.3.35")== 0){
                 ogs_debug("***CANOT Find gnode in MME List ***,inside IP");
-                gnode = ogs_gtp_node_find_by_addr(&sgwc_self()->pgw_s5c_list, &afrom);
+                gnode = ogs_gtp_node_find_by_addr(&sgwc_self()->pgw_s5c_list, afrom);
                 if (gnode) {
                     ogs_debug("***CANOT Find gnode in MME List ***,inside IP and found gnode");
                     e = sgwc_event_new(SGWC_EVT_S5C_MESSAGE);
